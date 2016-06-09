69e77806eea843203cc694a9d5ad325acb90bbe7
typ-c2sg <tyamaguchi@cloud-consulting.asia>
2016-06-09 08:10:27 +0800

    add import function

--- publics/views.py
+++ publics/views.py
@@ -11,28 +11,40 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    data_list = movie_list = None
+    movie_list = None
     try:
-        # loading data from url
-        url = "https://demo2697834.mockable.io/movies"
-        response = urllib.request.urlopen(url)
-        data_list = json.loads(response.read().decode('utf-8'))
-        # print(data_list)
-
-        for k in data_list['entries']:
-            print(k)
-
-        movie_list = Movie.objects.all()
-
-        # If we want to load data from file, we can use next code
-        # with open('publics/data.json', encoding='utf-8') as data_file:
-        #     data = json.loads(data_file.read())
-        # print(data)
+        movie_list = Movie.objects.all().order_by('?')[:10]
     except:
         logger.error(traceback.format_exc())
 
     context = {
-        'data_list': data_list['entries'],
         'movie_list': movie_list
     }
     return render(request, 'publics/index.html', context)
+
+
+def import_data():
+    # loading data from url
+    # url = "https://demo2697834.mockable.io/movies"
+    # response = urllib.request.urlopen(url)
+    # data_list = json.loads(response.read().decode('utf-8'))
+    # print(data_list)
+
+    with open('publics/data.json', encoding='utf-8') as data_file:
+        data_list = json.loads(data_file.read())
+    # print(data)
+
+    for data in data_list['entries']:
+        try:
+            Movie(
+                description=data['description'],
+                title=data['title'],
+                id=data['id'],
+                image=data['images'][0]['url'],
+                url=data['contents'][0]['url'],
+            ).save()
+        except:
+            print('Cannot create data')
+            print(data)
+            print(traceback.format_exc())
+    return
